5709f15c5fda2223896d7a6046ce37b013b3aac5
leeheeweon <ggoddii@naver.com>
2025-01-24 11:08:20 +0900

    potentiometer + LED

--- PotentionmeterInput/src/main.cpp
+++ PotentionmeterInput/src/main.cpp
@@ -1,13 +1,20 @@
 #include <Arduino.h>
 
+#define LED_PIN 13
+
 void setup() {
     // write your initialization code here
+    pinMode(LED_PIN, OUTPUT);
     Serial.begin(9600);
 }
+
 
 void loop() {
     // write your code here
     int R = analogRead(A0);
     Serial.print("potentiometer: ");
     Serial.println(R);
+
+    analogWrite(LED_PIN, R);
+
 }
