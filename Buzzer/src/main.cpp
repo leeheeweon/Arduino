669a4af74f894c7ef45b7abce16c30bfaec1be12
leeheeweon <ggoddii@naver.com>
2025-01-24 11:24:47 +0900

    Buzzer + LED

---
+++ Buzzer/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+#define ANALOG 0
+#define LED_PIN 13
+
+void setup() {
+    // write your initialization code here
+    pinMode(LED_PIN, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+    // write your code here
+    int R = analogRead(ANALOG);
+    if (R>100) {
+        digitalWrite(LED_PIN, HIGH);
+    } else {
+        digitalWrite(LED_PIN, LOW);
+    }
+    Serial.println(R);
+
+
+}
