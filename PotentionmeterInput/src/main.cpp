13194cfce5b04711676929bafb3e950a358827c5
leeheeweon <ggoddii@naver.com>
2025-01-24 10:37:08 +0900

    potentiometer

---
+++ PotentionmeterInput/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+void setup() {
+    // write your initialization code here
+    Serial.begin(9600);
+}
+
+void loop() {
+    // write your code here
+    int R = analogRead(A0);
+    Serial.print("potentiometer: ");
+    Serial.println(R);
+}
